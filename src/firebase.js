6e2807097ca78f6dd6121fb56cf2d99d336aa3ba
IE Tech <ietech.666@gmail.com>
2026-05-24 21:48:46 +0700

    fix firebase config typo

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSYHlblHN5-G_OZWkr0_xlXF_J3h8psLc",
+  apiKey: "AIzaSyDSYHIblHN5-G_OZWkr0_xIXF_J3h8psLc",
   authDomain: "amanah-daily.firebaseapp.com",
   projectId: "amanah-daily",
-  storageBucket: "amanah-daily.firebasestorage.app",
+  storageBucket: "amanah-daily.appspot.com",
   messagingSenderId: "1003932408144",
   appId: "1:1003932408144:web:4874696580ac5706157455"
 };
