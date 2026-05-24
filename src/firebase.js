f04919cfaaa2716be74c304fa5d3dd3bf0ceb6b9
IE Tech <ietech.666@gmail.com>
2026-05-24 21:31:43 +0700

    fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSYHLblHN5-G_0ZWkr0_xlXF_J3h8psLc",
+  apiKey: "AIzaSyDSYHlblHN5-G_OZWkr0_xlXF_J3h8psLc",
   authDomain: "amanah-daily.firebaseapp.com",
   projectId: "amanah-daily",
   storageBucket: "amanah-daily.firebasestorage.app",
   messagingSenderId: "1003932408144",
-  appId: "1:1003932408144:web:4874696508ac5706157455"
+  appId: "1:1003932408144:web:4874696580ac5706157455"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-
-export default app;
